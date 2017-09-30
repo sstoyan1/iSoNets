5804af54d44c55a3359a7ec331739fabb9d82bd4
sstoyan1 <sstoyan1@smail.uni-koeln.de>
2017-09-30 09:24:56 +0300

    Add files via upload

---
+++ iSoNets-python/secrets.py
@@ -0,0 +1,4 @@
+consumer_key = "z3Fx2lJBvpqCvMEQWCzUnmeTF"
+consumer_secret = "CdHkLrgE0HlRTOW2w7bZydY4AftjyRIMNPf3qVrSaOL6CPK3Lr"
+access_key = "904299311822974976-0y0zhZB2yLJkggN6UtRTyKq44zYpUkY"
+access_secret =  "904299311822974976-0y0zhZB2yLJkggN6UtRTyKq44zYpUkY"
