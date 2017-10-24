a07d5cd39d0b1e68c38de3e0760fcd776bbc2cad
sstoyan1 <sstoyan1@smail.uni-koeln.de>
2017-10-24 03:40:07 +0200

    Delete secrets.py
    Tokens were uploaded by mistake and deleted on the 24.10

--- iSoNets-python/secrets.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "z3Fx2lJBvpqCvMEQWCzUnmeTF"
-consumer_secret = "CdHkLrgE0HlRTOW2w7bZydY4AftjyRIMNPf3qVrSaOL6CPK3Lr"
-access_key = "904299311822974976-0y0zhZB2yLJkggN6UtRTyKq44zYpUkY"
-access_secret =  "904299311822974976-0y0zhZB2yLJkggN6UtRTyKq44zYpUkY"
